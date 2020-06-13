ff2ed1335ba675b70a372203eb70f77564094a80
Ishan Arora <ishanarora04@gmail.com>
2020-06-13 14:25:36 +0530

    Updates

---
+++ precache-manifest.86440bc6cc8774424a0f6eab97a88228.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0530078009baa2e3af1b2fdf2cbf0455",
+    "url": "/reactStopWatch/index.html"
+  },
+  {
+    "revision": "7ce9e0e1aca07349ea72",
+    "url": "/reactStopWatch/static/css/main.e2dd4494.chunk.css"
+  },
+  {
+    "revision": "972d8a1aa258ff739dd7",
+    "url": "/reactStopWatch/static/js/2.c6df8050.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactStopWatch/static/js/2.c6df8050.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ce9e0e1aca07349ea72",
+    "url": "/reactStopWatch/static/js/main.4e20c17c.chunk.js"
+  },
+  {
+    "revision": "83876df8be25dc36d2bf",
+    "url": "/reactStopWatch/static/js/runtime-main.c40996a2.js"
+  }
+]);
